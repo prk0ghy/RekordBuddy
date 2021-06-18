1420583ced6f0f2c769270fbee3390b70cac890b
Didier Malenfant <didier@rekordbuddy.org>
2021-06-18 12:01:34 +0200

    Fixed missing value for NXA_SOURCE_PATH

--- bin/macOS/bump.sh
+++ bin/macOS/bump.sh
@@ -56,6 +56,20 @@ function require {
     ok $1
 }
 
+function get_script_dir
+{
+     SOURCE="${BASH_SOURCE[0]}"
+     # While $SOURCE is a symlink, resolve it
+     while [ -h "$SOURCE" ]; do
+          DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+          SOURCE="$( readlink "$SOURCE" )"
+          # If $SOURCE was a relative symlink (so no "/" as prefix, need to resolve it relative to the symlink base directory
+          [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+     done
+     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+     echo "$DIR"
+}
+
 function check_git_clean {
     require git
 
@@ -87,6 +101,7 @@ function require_git_working_directory {
 }
 
 function export_paths {
+    export NXA_SOURCE_PATH=$(get_script_dir)/../..
     export PATH=$NXA_SOURCE_PATH/bin/macOS:$NXA_SOURCE_PATH/Vendor/Qt/macOS/bin:$PATH
     ok "NXA_SOURCE_PATH is $NXA_SOURCE_PATH"
 }
